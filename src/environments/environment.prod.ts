26af900d05c32a3f15694b372822f0716de93f92
Josh Murphy <joshmurph316@gmail.com>
2025-04-06 02:48:27 -0400

    disabled testing scripts temporarily, possible working build

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyC1cPDJ7cdLLmuA0Ln6Z1JRYx6TSCgs-FU",
+        authDomain: "team-builderv2.firebaseapp.com",
+        projectId: "team-builderv2",
+        storageBucket: "team-builderv2.firebasestorage.app",
+        messagingSenderId: "336577585276",
+        appId: "1:336577585276:web:8898d115eca663538069f9",
+        measurementId: "G-TTWF554WY8"
+      }
+  };
+  
