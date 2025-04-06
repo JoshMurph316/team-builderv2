ad9a2307d0d4365db552957f5641da1c8b4feb5d
Josh Murphy <joshmurph316@gmail.com>
2025-04-06 16:13:26 -0400

    adjustments made, action testing

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,12 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:3000'
-  };
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyC1cPDJ7cdLLmuA0Ln6Z1JRYx6TSCgs-FU",
+    authDomain: "team-builderv2.firebaseapp.com",
+    projectId: "team-builderv2",
+    storageBucket: "team-builderv2.firebasestorage.app",
+    messagingSenderId: "336577585276",
+    appId: "1:336577585276:web:8898d115eca663538069f9",
+    measurementId: "G-TTWF554WY8"
+  }
+};
